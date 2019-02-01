a3de5eed130e1f6e4c3dabca5b5598b1dd875f1c
Raffaello <Raphaelprmx@gmail.com>
2019-02-01 11:32:19 +0100

    Header

--- EFintusFrontEnd/src/app/app.module.ts
+++ EFintusFrontEnd/src/app/app.module.ts
@@ -1,14 +1,20 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponentComponent } from './components/header-component/header-component.component';
+import { HomeComponentComponent } from './components/home-component/home-component.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponentComponent,
+    HomeComponentComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
