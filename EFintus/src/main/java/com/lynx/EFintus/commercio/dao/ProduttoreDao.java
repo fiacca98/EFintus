fae5983fb4c348e40f504a8e427fa4362ebeadf9
Kawa <kawa.luca97@gmail.com>
2019-02-14 13:08:01 +0100

    ProduttoreDao e modifica a Dao<T>

---
+++ EFintus/src/main/java/com/lynx/EFintus/commercio/dao/ProduttoreDao.java
@@ -0,0 +1,105 @@
+package com.lynx.EFintus.commercio.dao;
+
+import java.beans.Statement;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+import com.lynx.EFintus.commercio.classes.Produttore;
+import com.lynx.EFintus.commercio.classes.Utente;
+
+public class ProduttoreDao extends GenericDao<Produttore> {
+
+    private String TABLE_NAME = "produttore";
+
+    @Override
+    public Produttore get(long id) throws SQLException {
+return null;
+    }
+
+    @Override
+    public List<Produttore> getAll() throws SQLException {
+        List<Produttore> produttori = new ArrayList<Produttore>();
+
+        Connection con = getConnection();
+        PreparedStatement ps = con.prepareStatement("select * from " + getTableName());
+        ResultSet rs = ps.executeQuery();
+        while (rs.next()) {
+            Produttore produttore = new Produttore(rs.getInt(1), rs.getString(2), rs.getString(3), rs.getString(4), rs.getString(5),
+                    rs.getString(6), rs.getString(7), rs.getString(8), rs.getString(9));
+            produttori.add(produttore);
+        }
+        con.close();
+
+        return produttori;
+    }
+
+    @Override
+    public void save(Produttore produttore) throws SQLException {
+        Connection con = getConnection();
+        PreparedStatement ps = con
+                .prepareStatement("insert into " + getTableAndColumns() + " values (?,?,?,?,?,?,?,?,?)");
+
+        ps.setInt(1, produttore.get_ID());
+        ps.setString(2, produttore.get_nome());
+        ps.setString(3, produttore.get_indirizzo());
+        ps.setString(4, produttore.get_citta());
+        ps.setString(5, produttore.get_cap());
+        ps.setString(6, produttore.get_telefono());
+        ps.setString(7, produttore.get_email());
+        ps.setString(8, produttore.get_descrizione());
+        ps.setString(9, produttore.get_partitaIVA());
+
+        int status = ps.executeUpdate();
+
+        con.close();
+    }
+
+    @Override
+    public void update(Produttore produttore) throws SQLException {
+        Connection con = getConnection();
+        PreparedStatement ps = con
+                .prepareStatement("update" + getTableName() + "set " +
+                        "nome="+produttore.get_nome()+
+                        "indirizzo="+produttore.get_indirizzo()+
+                        "citta="+produttore.get_citta()+
+                        "cap="+produttore.get_cap()+
+                        "telefono="+produttore.get_telefono()+
+                        "email="+produttore.get_email()+
+                        "descrizione="+produttore.get_descrizione()+
+                        "partitaIVA="+produttore.get_partitaIVA());
+
+        int status = ps.executeUpdate();
+
+        con.close();
+    }
+
+    @Override
+    public void delete(Produttore produttore) throws SQLException {
+        Connection con = getConnection();
+        PreparedStatement ps = con
+                .prepareStatement("DELETE FROM" + getTableName()+"WHERE ID="+produttore.get_ID());
+
+        int status = ps.executeUpdate();
+
+        con.close();
+    }
+
+    @Override
+    public Produttore fromResultSetToBean(Produttore produttore) {
+        return null;
+    }
+
+    @Override
+    public String getTableName() {
+        return TABLE_NAME;
+    }
+
+    @Override
+    public String getColumns() {
+        return "(ID, nome,indirizzo, citta, cap , telefono, email, descrizione, partitaIVA)";
+    }
+}
--- EFintus/src/main/java/com/lynx/EFintus/commercio/interfaces/Dao.java
+++ EFintus/src/main/java/com/lynx/EFintus/commercio/interfaces/Dao.java
@@ -11,7 +11,7 @@ public interface Dao<T> {
 
     void save(T t) throws SQLException;
 
-    void update(T t, String[] params) throws SQLException;
+    void update(T t) throws SQLException;
 
     void delete(T t) throws SQLException;
 
