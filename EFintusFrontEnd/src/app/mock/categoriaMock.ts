1102d5a95934e5e9645be897b1b93a112088903e
giacomo.mangugan <giacomomangugan98@gmail.com>
2019-02-14 13:00:42 +0100

    little fix in mock folder

--- EFintusFrontEnd/src/app/mock/categoriaMock.ts
+++ EFintusFrontEnd/src/app/mock/categoriaMock.ts
@@ -2,23 +2,23 @@ import { CategoryBean } from 'src/app/bean/categoryBean';
 
 
 export const CATEGORY1MOCK: CategoryBean[] = [
-    {id: 1, nome: "Categoria 1", parenteId: 0 },
-    {id: 2, nome: "Categoria 2", parenteId: 0 },
-    {id: 3, nome: "Categoria 3", parenteId: 0 },
-    {id: 4, nome: "Categoria 4", parenteId: 0 },
-    {id: 5, nome: "Categoria 5", parenteId: 0 },
-    {id: 6, nome: "Categoria 6", parenteId: 0 },
-    {id: 7, nome: "Categoria 7", parenteId: 1 },
-    {id: 8, nome: "Categoria 8", parenteId: 2 },
-    {id: 9, nome: "Categoria 9", parenteId: 3 },
-    {id: 10, nome: "Categoria 10", parenteId: 4 },
-    {id: 11, nome: "Categoria 11", parenteId: 5 },
-    {id: 12, nome: "Categoria 12", parenteId: 6 },
-    {id: 13, nome: "Categoria 13", parenteId: 1 },
-    {id: 14, nome: "Categoria 14", parenteId: 2 },
-    {id: 15, nome: "Categoria 15", parenteId: 3 },
-    {id: 16, nome: "Categoria 16", parenteId: 4 },
-    {id: 17, nome: "Categoria 17", parenteId: 5 },
-    {id: 18, nome: "Categoria 18", parenteId: 6 }
+    {id: 1, nome: 'Categoria 1', parenteId: 0 },
+    {id: 2, nome: 'Categoria 2', parenteId: 0 },
+    {id: 3, nome: 'Categoria 3', parenteId: 0 },
+    {id: 4, nome: 'Categoria 4', parenteId: 0 },
+    {id: 5, nome: 'Categoria 5', parenteId: 0 },
+    {id: 6, nome: 'Categoria 6', parenteId: 0 },
+    {id: 7, nome: 'Categoria 7', parenteId: 1 },
+    {id: 8, nome: 'Categoria 8', parenteId: 2 },
+    {id: 9, nome: 'Categoria 9', parenteId: 3 },
+    {id: 10, nome: 'Categoria 10', parenteId: 4 },
+    {id: 11, nome: 'Categoria 11', parenteId: 5 },
+    {id: 12, nome: 'Categoria 12', parenteId: 6 },
+    {id: 13, nome: 'Categoria 13', parenteId: 1 },
+    {id: 14, nome: 'Categoria 14', parenteId: 2 },
+    {id: 15, nome: 'Categoria 15', parenteId: 3 },
+    {id: 16, nome: 'Categoria 16', parenteId: 4 },
+    {id: 17, nome: 'Categoria 17', parenteId: 5 },
+    {id: 18, nome: 'Categoria 18', parenteId: 6 }
 
 ]
