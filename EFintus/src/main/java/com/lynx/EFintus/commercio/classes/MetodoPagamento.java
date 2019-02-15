1fa458852d042d48015b57ad415ea4838c001a07
Luigi <lui.gi98@ive.it>
2019-02-15 09:53:51 +0100

    metodo pagamento class

---
+++ EFintus/src/main/java/com/lynx/EFintus/commercio/classes/MetodoPagamento.java
@@ -0,0 +1,19 @@
+package com.lynx.EFintus.commercio.classes;
+
+public class MetodoPagamento {
+
+    private String metodoPagamento;
+
+    public MetodoPagamento(String metodoPagamento) {
+	this.metodoPagamento = metodoPagamento;
+    }
+
+    public String getMetodoPagamento() {
+	return metodoPagamento;
+    }
+
+    public void setMetodoPagamento(String metodoPagamento) {
+	this.metodoPagamento = metodoPagamento;
+    }
+
+}
