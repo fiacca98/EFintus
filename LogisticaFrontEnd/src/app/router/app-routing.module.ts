e773fd05b78ab015b7fe03bc40666ebceff21d1c
SimoneBergomi <bergomi.simone@gmail.com>
2019-02-28 11:25:04 +0100

    home e modifica navbar

--- LogisticaFrontEnd/src/app/router/app-routing.module.ts
+++ LogisticaFrontEnd/src/app/router/app-routing.module.ts
@@ -8,8 +8,10 @@ import { FornitoriComponent } from '../components/fornitori/fornitori.component'
 import { ProduttoreComponent } from '../components/produttore/produttore.component';
 import { LavoratoreComponent } from '../components/lavoratore/lavoratore.component';
 import { CorriereComponent } from '../components/corriere/corriere.component';
+import { HomeComponent } from '../components/home/home.component';
 
 const routes: Routes = [
+  {path: "home", component: HomeComponent},
   {path:'ordini', component: OrdersComponent},
   {path: 'magazzini', component: MagazziniComponent},
   {path: 'fornitori', component: FornitoriComponent},
